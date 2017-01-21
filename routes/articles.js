625001ccdd90265a8440f58a47bd68bb7d995f5e
Danilova Elena <Danilova Elena>
2017-01-21 18:26:47 +0300

    Refactor issues

--- routes/articles.js
+++ routes/articles.js
@@ -2,15 +2,24 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
 var upload = multer({ dest: './public/uploads/' });
+var config = require('../configs/config.json');
 
 var ArticleController = require('../controllers/ArticleController');
 
 router.get('/', function(req, res) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
 	ArticleController.getLastArticles().then((articles) => {
-		return res.status(200).json(articles);
+		const newArticles = [];
+		articles.forEach((article, index) => {
+			newArticles[index] = {
+				author: article.author,
+				description: article.description,
+				title: article.title,
+				link: article.link,
+				imageUrl: `http://localhost:${config.port}/uploads/${article.imageHash}`
+			};
+		});
+
+		return res.status(200).json(newArticles);
 	})
 	.catch((error) => {
 		console.log(error);
@@ -36,7 +45,7 @@ router.post('/', upload.single('image'), function(req, res) {
         title: req.body.title,
         description: req.body.description,
         link: req.body.link,
-        imageHash: req.file.filename
+        imageHash: req.file.filename 
     };
 
 	ArticleController.addArticle(article).then(() => {
